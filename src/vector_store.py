02a0a497b01b46f608b55b68abcaced5897516e1
0XSoumya <soumyasahoo.2907@gmail.com>
2025-04-02 23:48:15 +0530

    Add vector store initialization

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,1 +1,7 @@
 # Code for managing the vector database
+import chromadb
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+chroma_client = chromadb.PersistentClient(path="./data/chroma_db")
+vectorstore = Chroma(client=chroma_client, embedding_function=OpenAIEmbeddings())
